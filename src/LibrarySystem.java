ef00b407a7a2552b0c23e51d0c0ce821d189a705
Tentei06 <tentei06@gmail.com>
2026-06-07 14:11:52 -0600

    updated user menu design

--- src/LibrarySystem.java
+++ src/LibrarySystem.java
@@ -64,17 +64,17 @@ public class LibrarySystem
     public static void printMenu() // prints themed menu (going for a dark academia theme)
     {
         System.out.println();
-        System.out.println("╭────────────────────────────────╮");
-        System.out.println("│        Raven Hall Library      │");
-        System.out.println("│      Collection Management     │");
-        System.out.println("╰────────────────────────────────╯");
-        System.out.println("[1] Add Book");
-        System.out.println("[2] Borrow Book");
-        System.out.println("[3] Return Book");
-        System.out.println("[4] Search by Title");
-        System.out.println("[5] Print All Books");
-        System.out.println("[6] Exit");
-        System.out.println("──────────────────────────────────");
+        System.out.println("====================================");
+        System.out.println("|        Raven Hall Library        |");
+        System.out.println("|     Collection Management        |");
+        System.out.println("====================================");
+        System.out.println("| [1] Add Book                     |");
+        System.out.println("| [2] Borrow Book                  |");
+        System.out.println("| [3] Return Book                  |");
+        System.out.println("| [4] Search by Title              |");
+        System.out.println("| [5] Print All Books              |");
+        System.out.println("| [6] Exit                         |");
+        System.out.println("====================================");
     }
 
     // collects book details from the user and adds a new book 
