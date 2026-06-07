8c9fd714091a300872e5e40ef99386a560bf79cb
Tentei06 <tentei06@gmail.com>
2026-06-07 14:12:47 -0600

    more alignment changes

--- src/LibrarySystem.java
+++ src/LibrarySystem.java
@@ -66,7 +66,7 @@ public class LibrarySystem
         System.out.println();
         System.out.println("====================================");
         System.out.println("|        Raven Hall Library        |");
-        System.out.println("|     Collection Management        |");
+        System.out.println("|      Collection Management       |");
         System.out.println("====================================");
         System.out.println("| [1] Add Book                     |");
         System.out.println("| [2] Borrow Book                  |");
